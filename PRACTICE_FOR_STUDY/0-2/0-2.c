3b1c27e27f842bdfdb9d67113394341d7457434e
thdalswl1013 <thdalswl1013@naver.com>
2023-03-18 22:34:25 +0900

    upload 0318

--- PRACTICE_FOR_STUDY/0-2/0-2.c
+++ PRACTICE_FOR_STUDY/0-2/0-2.c
@@ -1,3 +1,4 @@
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
@@ -5,9 +6,22 @@
 
 int main(void)
 {
+	char sentence[100];
 
+	printf("문자열을 입력하세요 : ");
+	scanf("%s", &sentence);
 
+	int count = 0;
 
+	for (int i = 0; i < strlen(sentence)-1; i++)
+	{
+		//printf("%c\n", sentence[i]);
+		
+		if (sentence[i] == 'a' && sentence[i+1] == 'b')
+				count += 1;
+	}
+
+	printf("반복 횟수 : %d", count);
 
 	return 0;
 }
