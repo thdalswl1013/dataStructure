5cf1315c103aeddbb3c486010a6dce7dc0a1b880
thdalswl1013 <thdalswl1013@naver.com>
2023-03-19 11:54:32 +0900

    upload 0319

--- PRACTICE_FOR_STUDY/1-4/1-4.c
+++ PRACTICE_FOR_STUDY/1-4/1-4.c
@@ -10,10 +10,22 @@ int main(void)
 	char sentence[100];
 
 	printf("문자열 입력 : ");
-	scanf("%s", &sentence);
+	gets(sentence,sizeof(sentence)); 	//scanf("%s", &sentence);
+
+
 
 	printf("자음만 대문자로 변환 결과 : ");
 
+	for (int i = 0; i < strlen(sentence); i++)
+	{
+		if (sentence[i] == 'a' || sentence[i] == 'e' || sentence[i] == 'i' || sentence[i] == 'o' || sentence[i] == 'u')
+			sentence[i] = sentence[i];
+
+		else
+			sentence[i] = toupper(sentence[i]);
+	}
+
+	printf("%s", sentence);
 
 	return 0;
 }
