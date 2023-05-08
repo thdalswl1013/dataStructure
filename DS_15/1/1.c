3b87563a617554fafa73d25911f89aad88bfbd43
thdalswl1013 <thdalswl1013@naver.com>
2023-05-08 15:36:14 +0900

    upload 0508

--- DS_15/1/1.c
+++ DS_15/1/1.c
@@ -3,7 +3,99 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main(void)
+int* winnerTree;
+int* sorted;
+int key, seed;
+int winner;
+
+int findwinner(int k);
+void inorder(int i, int k);
+
+int main()
 {
 
+	printf("<<<<< sorting with winner tree >>>>>\n\n");
+	printf("the number of keys(4, 8, 16 or 32 as a power of 2) >> ");
+	scanf("%d", &key);
+
+	winnerTree = calloc(key * 2, sizeof(int));
+	sorted = calloc(key, sizeof(int));
+
+	printf("value of seed >> ");
+	scanf("%d", &seed);
+
+	for (int i = 0; i < key; i++)
+	{
+		winnerTree[i + key] = rand() % 500 + 1;
+		printf("%5d", winnerTree[i + key]);
+	}
+
+	printf("\n\n");
+	printf("initialization of min-winner tree\n\n");
+	winner = findwinner(key);
+
+	printf("inorder traversal for min-winner tree\n");
+	inorder(1, key);
+
+	sorted[0] = winnerTree[winner];
+	winnerTree[winner] = 9999;
+
+	for (int i = 1; i < key; i++) 
+	{
+		winner = findwinner(key);
+		sorted[i] = winnerTree[winner];
+		winnerTree[winner] = 9999;
+
+		if (i == 1) 
+		{
+			printf("\nWinner Tree inorder traversal after find 2 winner\n");
+			inorder(1, key);
+		}
+	}
+
+	printf("\n\n");
+	printf("Sorting with min-winner tree...\n\n");
+	printf("sorted result\n");
+	
+	for (int i = 0; i < key; i++) 
+		printf("%5d", sorted[i]);
+
+	return 0;
+}
+
+
+int findwinner(int k)
+{
+	int left, right;
+
+	for (int i = k / 2; i >= 1;) 
+	{
+		left = winnerTree[i * 2];
+		right = winnerTree[i * 2 + 1];
+
+		winnerTree[i] = (left > right) ? right : left;
+
+		if (++i % 4 == 0)
+			i /= 4;
+		else if (i == 2)
+			break;
+	}
+
+	winnerTree[0] = winnerTree[1];
+
+	for (int i = 0; i < k; i++) 
+	{
+		if (winnerTree[0] == winnerTree[k + i])
+			return k + i;
+	}
+}
+
+void inorder(int i, int k)
+{
+	if (i < k * 2)
+	{
+		inorder(i * 2, k);
+		printf("%5d", winnerTree[i]);
+		inorder(i * 2 + 1, k);
+	}
 }
--- DS_15/2/2.c
+++ DS_15/2/2.c
@@ -3,7 +3,112 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main(void)
-{
+#define RUN 8
+#define RUN_NUM 10
 
+int winnerTree[RUN * 2] = { 0, };
+int sorted[RUN * RUN_NUM] = { 0, };
+int arr[RUN][RUN_NUM] = { 0, };
+int indexarr[RUN] = { 0, };
+
+
+int findwinner();
+int init_winnerTree();
+void sort_winnerTree(int i, int index);
+void levelorder();
+
+int main()
+{
+	FILE* fp;
+	fp=fopen("input.txt", "r");
+
+	printf("<<<<<<<< run data >>>>>>>>\n\n");
+
+	for (int i = 0; i < RUN; i++) 
+	{
+		printf("%d-th run: \n", (i + 1));
+		for (int j = 0; j < RUN_NUM; j++) 
+		{
+			fscanf(fp, "%d", &arr[i][j]);
+			printf("%5d", arr[i][j]);
+		}
+		printf("\n");
+	}
+
+	init_winnerTree();
+	int index = 0;
+
+	for (int i = 0; i < RUN * RUN_NUM; i++)
+	{
+		sort_winnerTree(i, index);
+		if (i == 0)
+		{
+			printf("\nLevel-order traversal for initial min-winner tree\n");
+			levelorder();
+
+			printf("\nsorting with min-winner tree...");
+			printf("\n\nsorted result");
+		}
+
+		if (i % 8 == 0)
+			printf("\n");
+		printf("%4d", sorted[i]);
+	}
+
+	printf("\n");
+	return 0;
+}
+
+
+int findwinner()
+{
+	int left, right;
+
+	for (int i = RUN / 2; i >= 1;) 
+	{
+		left = winnerTree[i * 2];
+		right = winnerTree[i * 2 + 1];
+
+		winnerTree[i] = (left > right) ? right : left;
+
+		if (++i % 4 == 0)
+			i /= 4;
+		else if (i == 2)
+			break;
+	}
+
+	winnerTree[0] = winnerTree[1];
+
+	for (int i = 0; i < RUN; i++)
+	{
+		if (winnerTree[0] == winnerTree[RUN + i])
+			return RUN + i;
+	}
+}
+
+int init_winnerTree()
+{
+	for (int i = 0; i < RUN; i++) 
+	{
+		indexarr[i] = 0;
+		winnerTree[i + RUN] = arr[i][0];
+	}
+}
+
+
+void levelorder()
+{
+	for (int i = 1; i < RUN * 2; i++) 
+		printf("%3d", winnerTree[i]);
+	printf("\n");
+}
+
+void sort_winnerTree(int i, int index)
+{
+	index = findwinner();
+	sorted[i] = winnerTree[index];
+	winnerTree[index] = arr[index - RUN][++indexarr[index - RUN]];
+
+	if (indexarr[index - RUN] == RUN_NUM)
+		winnerTree[index] = 9999;
 }
