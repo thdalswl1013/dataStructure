938acb90b24de4e7aa87f15eb9511bd7cd291af5
thdalswl1013 <thdalswl1013@naver.com>
2023-06-09 15:39:00 +0900

    upload 0609

--- PRACTICE_FOR_FINAL/12-1/12-1.c
+++ PRACTICE_FOR_FINAL/12-1/12-1.c
@@ -1,11 +1,126 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct node* treePointer;
+typedef struct node
+{
+	char data;
+	treePointer leftchild;
+	treePointer rightchild;
+};
+treePointer stack[80];
+
+int top = -1;
+
+void inorder(treePointer ptr);
+void preorder(treePointer ptr);
+void postorder(treePointer ptr);
+
+void push(treePointer ptr); 
+treePointer pop();
+
 
 int main(void)
 {
+	treePointer tree = NULL;
+	char input;
+	FILE* fp;
+	fp = fopen("input.txt", "r");
 
+	printf("the length of input string should be less than 80\n");
+	printf("input string(postfix expression) : ");
+
+	while (!feof(fp))
+	{
+		fscanf(fp, "%c", &input);
+		printf("%c", input);
+		treePointer temp = NULL;
+		if (input == '+' || input == '-' || input == '/' || input == '*' || input == '%')
+		{
+			temp = malloc(sizeof(*temp));
+			temp->data = input;
+			temp->leftchild = NULL;
+			temp->rightchild = NULL;
+
+			temp->rightchild = pop();
+			temp->leftchild = pop();
+			push(temp);
+		}
+
+		else
+		{
+			temp = malloc(sizeof(*temp));
+			temp->data = input;
+			temp->leftchild = NULL;
+			temp->rightchild = NULL;
+
+			push(temp);
+		}
+	}
+
+	tree = pop();
+
+
+
+	printf("\ncreating its binary tree\n");
+
+	printf("\nInorder traversal	: ");
+	inorder(tree);
+
+	printf("\nPreorder traversal	: ");
+	preorder(tree);
+
+	printf("\nPostorder traversal	: ");
+	postorder(tree);
+	printf("\n");
 
 	return 0;
+}
+
+
+void inorder(treePointer ptr)
+{
+	if (ptr)
+	{
+		inorder(ptr->leftchild);
+		printf("%c", ptr->data);
+		inorder(ptr->rightchild);
+	}
+}
+
+void preorder(treePointer ptr)
+{
+	if (ptr)
+	{
+		printf("%c", ptr->data);
+		preorder(ptr->leftchild);
+		preorder(ptr->rightchild);
+	}
+}
+
+void postorder(treePointer ptr)
+{
+	if (ptr)
+	{
+		postorder(ptr->leftchild);
+		postorder(ptr->rightchild);
+		printf("%c", ptr->data);
+	}
+}
+
+
+void push(treePointer ptr)
+{
+	stack[++top] = ptr;
+}
+
+treePointer pop()
+{
+	treePointer temp = NULL;
+
+	if (top >= 0)
+		temp = stack[top--];
+	return temp;
 }
